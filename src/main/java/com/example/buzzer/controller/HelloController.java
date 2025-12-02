f73bbd5e7c21dcb8d46613f1eea19b84266fa804
wangchaochao <wangchaochao@58.com>
2025-12-02 16:54:49 +0800

    功能开发

--- src/main/java/com/example/buzzer/controller/HelloController.java
+++ src/main/java/com/example/buzzer/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.example.buzzer.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HelloController {
@@ -9,6 +10,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String helloWorld() {
         return "Hello World!";
+    }
+    
+    @GetMapping("/")
+    public ModelAndView index() {
+        return new ModelAndView("redirect:/word-guess-game.html");
     }
 
 }
